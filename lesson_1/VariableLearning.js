e0bc5f10235a1b9946b41b53c6f1d52f2edfb8e8
ltphuong <jackey.phuong@gmail.com>
2024-01-15 16:52:17 +0700

    init commit

---
+++ lesson_1/VariableLearning.js
@@ -0,0 +1,11 @@
+let myName = 'Teo'
+myName = 18
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Infinity > 1.7976931348623157e+309);
+
+let amIRight = true;
+console.log(typeof amIRight);
+console.log(typeof myName);
+console.log(1.7976931348623157e+309);

e415d06917119125cac20fdc440f850fb40f833f
ltphuong <jackey.phuong@gmail.com>
2024-01-31 15:15:06 +0700

    Add lesson 2 and lab 2

---
+++ lesson/lesson_2/IfElseStatement.js
@@ -0,0 +1,15 @@
+const EXPECTED_TIME = 7;
+let arrivalTime = 8;
+
+
+//if (value){}
+// ==, ===, !=, !==, >, <, >=, <=
+let isHeOnTime = arrivalTime === EXPECTED_TIME
+if(isHeOnTime){
+    console.log("Let's talk");
+}
+else{
+    console.log("Write a letter");
+}
+
+console.log("End program!");
---
+++ lesson/lesson_2/multipleIfElse.js
@@ -0,0 +1,18 @@
+// <18 | 18-55 | 55
+
+let clientAge = 18;
+if(clientAge < 18){
+    console.log("Con nit ma doi uong bia, khong ban!");
+}
+else if(clientAge >= 18 && clientAge <= 55){
+    console.log(("Unlimited"));
+}
+else{
+    console.log("2 chai");
+}
+
+/**
+ * firstCondition && secondCondition ==> true ONLY them both are true
+ * firstCondition || secondCondition ==> false ONLY them both are false
+ * 
+ */

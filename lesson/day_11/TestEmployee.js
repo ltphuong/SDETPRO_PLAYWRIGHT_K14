482271af4e8e4a8604af788dfc5fe231e43f100f
phuongthanh <jackey.phuong@gmail.com>
2024-03-06 23:51:45 +0700

    add day_11

---
+++ lesson/day_11/TestEmployee.js
@@ -0,0 +1,13 @@
+const Employee = require('./Employee')
+const SalaryController = require('./SalaryController')
+
+// Data
+const teo = new Employee(50);
+const ti = new Employee(40);
+
+// Controller
+const salaryController = new SalaryController();
+const totalSalary = salaryController.getTotalSalary([teo, ti]);
+
+// View
+console.log(totalSalary);
